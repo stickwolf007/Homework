cc95ad48f6759a7199ad57b5998063cfa9aa4754
Олег <oleg_stick@mail.ru>
2024-08-12 20:11:36 +0300

    new

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anton': 1994, 'Alina': 1990, 'Oleg': 1991}
+print(my_dict)
+print(my_dict['Anton'])
+print(my_dict.get('Anna'))
+my_dict.update({'Milena': 2019, 'Alexey': 1972})
+print(my_dict.pop('Alina'))
+print(my_dict)
+
+
+
+my_set={5,3,3,5,8,9,6,4,7,8,9}
+print(my_set)
+my_set.update({10, 12})
+my_set.remove(3)
+print(my_set)
